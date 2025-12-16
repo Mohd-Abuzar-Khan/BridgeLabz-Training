ddf2a4264152b1e406eac185b8199c0800ffff2d
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-16 17:01:11 +0530

    [Abuzar] : added a program for finding Simple Interest

---
+++ gcr-codebase/programming-prerequisite/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest{
+	public static void main(Strings[] args){
+		Scanner sc = new Scanner (System.in);
+		int principal = sc.nextInt();
+		int rate = sc.nextInt();
+		int time = sc.nextInt();
+		double simpleInterest
+	}
+}
