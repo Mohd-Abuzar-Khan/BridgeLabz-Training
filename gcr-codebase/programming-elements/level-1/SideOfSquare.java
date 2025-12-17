0042aab23ef5895ceb76ea2392c5e10feecb2d22
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-17 22:56:36 +0530

    [Abuzar] : added a program to find side of square using sites

---
+++ gcr-codebase/programming-elements/level-1/SideOfSquare.java
@@ -0,0 +1,20 @@
+// importing dependencies for taking input
+import java.util.Scanner;
+
+// Created a SideOfSquare class to find side of square using it's perimeter
+public class SideOfSquare{
+	public static void main(String[] args){
+		
+		// Taking perimeter as input
+		Scanner input = new Scanner(System.in);
+		double perimeter = input.nextDouble();
+		
+		// Calculating Side of Square using Perimeter
+		double side = perimeter / 4;
+		
+		// Printing Side of Square
+		System.out.println("The length of the side is " + side +" whose perimeter is " + perimeter);
+		
+		input.close(); // Closing the Scanner Object
+	}
+}
