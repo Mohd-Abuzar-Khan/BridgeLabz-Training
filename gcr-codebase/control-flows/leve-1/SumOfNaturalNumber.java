865e3d8cc6f2171d018084a33688fa9b43192794
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-18 15:53:29 +0530

    [Abuzar] : added a program to find the sum of all Natural number to the given number

---
+++ gcr-codebase/control-flows/leve-1/SumOfNaturalNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumber{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+	
+			// Taking number as Input
+			System.out.println("Enter Number:");
+			int num = sc.nextInt();
+			
+			//Calculating Sum of the Natural Number using Formula
+			int sumFormula = (num *(num + 1))/2;
+			int sum = 0;
+			
+			// Calculating Sum Using While Loop
+			while(num != 0){
+				sum = sum + num;
+				num = num - 1;
+			}
+			
+			// Checking if counter reaches to 1 using for loop
+			System.out.println(sumFormula == sum);
+			
+		sc.close(); // Closing the Scanner Object
+	}
+}
