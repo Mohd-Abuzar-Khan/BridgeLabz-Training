1255cfea3418d58b9c0afb0208893927c6f7cb7d
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-20 00:48:33 +0530

    [Abuzar] : Added a program to find factors of a number using arrays

--- gcr-codebase/arrays/level-1/FactorArray.java
+++ gcr-codebase/arrays/level-1/FactorArray.java
@@ -1,50 +1,45 @@
 import java.util.Scanner;
 
-class MeanOfHeight {
-   public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-	  
-	  // Initalizing array for taking numbers
-	  System.out.print("Enter a number: ");
-	  int num = input.nextInt();
-	  
-	  //creating max factor of the number
-	  int maxFactor = 10;
-	  int factorIndex = 0;
-	  int factor[] = new int[maxFactor];
-	  
-	  
-	  //Finding factors
-	  for (int i =0; i < num ; i ++){
-		  
-		  if (num % i == 0){
-			  
-			  factor[factorIndex] = i;
-			  factorIndex = factorIndex + 1;
-			  
-		  }
-		  
-		  if (factorIndex == maxFactor){
-			  
-		  }
-	  }
-	  
-	  
-      // Take input for a age of array using loop
-	  for (int i = 0; i < arr.length  ; i++ ){
-		  System.out.print("Enter height of a Athlete: ");
-		  double height = input.nextDouble();
-		  sum = sum + height;
-	  }
-	  
-	  double mean = sum / arr.length;
-	  
-	  
-	  //Displaying the result
-	  System.out.println("The MeanS of the height of all the given Athlete is " + sum);
+public class FactorArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        // Take input
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
 
-      // Close the Scanner Object
-      input.close();
-   }
+        // Initialize array variables
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+
+        // Find factors
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+
+                // Resize array if full
+                if (index == maxFactor) {
+                    maxFactor = maxFactor * 2;
+                    int[] temp = new int[maxFactor];
+
+                    for (int j = 0; j < index; j++) {
+                        temp[j] = factors[j];
+                    }
+
+                    factors = temp;
+                }
+
+                factors[index] = i;
+                index++;
+            }
+        }
+
+        // Display factors
+        System.out.println("Factors of " + num + " are:");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+
+        sc.close();
+    }
 }
