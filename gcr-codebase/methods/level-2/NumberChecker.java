a1624545725ffe669e5f58e4d781c98d721939b7
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-21 02:03:35 +0530

    [Abuzar] : Added a program to check wheter a number is negative , positive or zero

---
+++ gcr-codebase/methods/level-2/NumberChecker.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class NumberChecker {
+
+    // Method to check whether number is positive or negative
+    public static boolean isPositive(int num) {
+        return num >= 0;
+    }
+
+    // Method to check whether number is even or odd
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    // Method to compare two numbers
+    // Returns 1 if num1 > num2, 0 if equal, -1 if num1 < num2
+    public static int compare(int num1, int num2) {
+        if (num1 > num2) {
+            return 1;
+        } else if (num1 == num2) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Array to store 5 numbers
+        int[] numbers = new int[5];
+
+        // Taking input
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        // Checking positive/negative and even/odd
+        for (int i = 0; i < numbers.length; i++) {
+            int num = numbers[i];
+
+            if (isPositive(num)) {
+                if (isEven(num)) {
+                    System.out.println(num + " is Positive and Even");
+                } else {
+                    System.out.println(num + " is Positive and Odd");
+                }
+            } else {
+                System.out.println(num + " is Negative");
+            }
+        }
+
+        // Comparing first and last elements
+        int result = compare(numbers[0], numbers[numbers.length - 1]);
+
+        if (result == 1) {
+            System.out.println("First element is greater than last element");
+        } else if (result == 0) {
+            System.out.println("First element is equal to last element");
+        } else {
+            System.out.println("First element is less than last element");
+        }
+
+        sc.close(); // Closing Scanner object
+    }
+}
