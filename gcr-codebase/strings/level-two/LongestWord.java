6ebcac21fc028be4a053f8ec802ed1e84f23d9c1
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-24 13:27:41 +0530

    [Abuzar] : Added a program for find the longest word in a string

---
+++ gcr-codebase/strings/level-two/LongestWord.java
@@ -0,0 +1,32 @@
+// Checking for the longest word in a text
+import java.util.Scanner;
+
+public class LongestWord {
+	
+	// Created a method to check for the longest word in a text
+	public static String longestWord(String str) {
+		String[] words = str.split(" ");
+		String longest = "";
+
+		for (String word : words) {
+			if (word.length() > longest.length())
+				longest = word;
+		}
+		return longest;
+	}
+	
+	public static void main(String[] args) {
+		
+		//Taking text as an input
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a text to find longest word");
+		String str = sc.nextLine();
+		
+		//Removing duplicate
+		String result = longestWord(str);
+		
+		// Displaying result
+		System.out.println("The string " + str + " has the longest word " + result );
+		sc.close();
+	}
+}
