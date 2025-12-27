298dbdf062d7efc5b6d825b0bec9c84da0a1ca74
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-27 23:33:07 +0530

    [Abuzar] : Added a program to display details about course and update institute name

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/levelone/Course.java
@@ -0,0 +1,31 @@
+package com.constructor.levelone;
+
+public class Course {
+
+	String courseName;
+	int duration;
+	double fee;
+	
+	static String instituteName = "Technocrats";
+	
+	Course(String courseName, int duration, double fee) {
+        this.courseName = courseName;
+        this.duration = duration;
+        this.fee = fee;
+    }
+	
+    // Instance Method
+    void displayCourseDetails() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " months");
+        System.out.println("Fee: ₹" + fee);
+        System.out.println("Institute: " + instituteName);
+        System.out.println("----------------------------------");
+    }
+
+    // Class Method (Static Method)
+    static void updateInstituteName(String newName) {
+        instituteName = newName;
+    }
+
+}
