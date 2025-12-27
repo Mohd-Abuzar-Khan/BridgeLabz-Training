3b9a1dd0bce2191f0016514f92051b65345f9802
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-27 23:31:31 +0530

    [Abuzar] : Added a program to display radius of a circle using constructor chaining

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/levelone/Circle.java
@@ -0,0 +1,17 @@
+package com.constructor.levelone;
+
+public class Circle {
+
+	double radius;
+	
+	//Default  Constructor
+	Circle() {
+		this(50.0);
+	}
+	
+	//Parameterized Constructor
+	Circle(double radius) {
+		this.radius = radius;
+	}
+
+}
