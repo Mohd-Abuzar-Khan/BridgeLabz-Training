e48aa673cc1a14582f850319f73b207c75fcf037
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-31 13:08:02 +0530

    [Abuzar] : Added a program to represent Company Department

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/levelone/CompanyDepartment.java
@@ -0,0 +1,31 @@
+package com.objectmodeling.levelone;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyDepartment {
+    private String departmentName;
+    private List<CompanyEmployee> employees;
+
+    public CompanyDepartment(String departmentName) {
+        this.departmentName = departmentName;
+        this.employees = new ArrayList<>();
+        System.out.println("Department created: " + departmentName);
+    }
+
+    public void addEmployee(String name, int id) {
+        employees.add(new CompanyEmployee(name, id));
+    }
+
+    public void showEmployees() {
+        System.out.println("Employees in " + departmentName + ":");
+        for (CompanyEmployee emp : employees) {
+            emp.displayEmployee();
+        }
+    }
+
+    public void closeDepartment() {
+        System.out.println("Closing Department: " + departmentName);
+        employees.clear(); // All employees removed
+    }
+}
+
