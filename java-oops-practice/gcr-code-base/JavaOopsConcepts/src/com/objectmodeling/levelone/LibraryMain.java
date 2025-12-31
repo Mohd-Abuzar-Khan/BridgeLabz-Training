0216ea37bfa3ae80ea1360cf2e3e4558f2f62302
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-31 10:16:08 +0530

    [Abuzar] : Added a program for Main method to run and test maneging a Library

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/levelone/LibraryMain.java
@@ -0,0 +1,27 @@
+package com.objectmodeling.levelone;
+
+public class LibraryMain {
+
+    public static void main(String[] args) {
+
+        // Creating Book objects (independent entities)
+        Book book1 = new Book("How to win friend", "Daniel Carnegie");
+        Book book2 = new Book("Clean Code", "Robert C. Martin");
+        Book book3 = new Book("Deep Work", "Cal Newport");
+
+        // Creating Library objects
+        Library centralLibrary = new Library("Central Library");
+        Library cityLibrary = new Library("City Library");
+
+        // Adding books to libraries (Aggregation)
+        centralLibrary.addBook(book1);
+        centralLibrary.addBook(book2);
+
+        cityLibrary.addBook(book2);
+        cityLibrary.addBook(book3);
+
+        // Display books in each library
+        centralLibrary.showBooks();
+        cityLibrary.showBooks();
+    }
+}
