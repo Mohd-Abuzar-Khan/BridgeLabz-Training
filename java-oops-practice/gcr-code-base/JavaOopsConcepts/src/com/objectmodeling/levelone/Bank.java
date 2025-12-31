ee646ad8a3f1479313eba28b79bcce1d0e193704
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-31 10:06:19 +0530

    [Abuzar] : Added a Class Bank to Manage all the Bank.

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/levelone/Bank.java
@@ -0,0 +1,33 @@
+package com.objectmodeling.levelone;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String bankName;
+    private ArrayList<Account> accounts;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+        this.accounts = new ArrayList<>();
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    // Association: Bank opens account for customer
+    public Account openAccount(Customer customer, int accountNumber, double balance) {
+        Account account = new Account(accountNumber, balance, this);
+        accounts.add(account);
+        customer.addAccount(account);
+        return account;
+    }
+
+    public void showAllAccounts() {
+        System.out.println("\nAccounts in " + bankName + ":");
+        for (Account acc : accounts) {
+            acc.displayAccountInfo();
+        }
+    }
+}
