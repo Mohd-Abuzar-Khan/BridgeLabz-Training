dc4cb75bbbc1deb1ba7498350e440c9b0217c18c
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2025-12-31 13:11:46 +0530

    [Abuzar] : Added a program to run and test course Managment System

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/levelone/CourseManagmentMain.java
@@ -0,0 +1,39 @@
+package com.objectmodeling.levelone;
+
+public class CourseManagmentMain {
+    public static void main(String[] args) {
+
+        // Create school
+        School school = new School("Anand Vihar High School");
+
+        // Create students
+        Student s1 = new Student("Nageshwar", 101);
+        Student s2 = new Student("Shilpa", 102);
+
+        // Create courses
+        SchoolCourse math = new SchoolCourse("Mathematics");
+        SchoolCourse science = new SchoolCourse("Science");
+
+        // Add students to school (Aggregation)
+        school.addStudent(s1);
+        school.addStudent(s2);
+
+        // Enroll students in courses (Association)
+        s1.enrollCourse(math);
+        s1.enrollCourse(science);
+
+        s2.enrollCourse(science);
+
+        // Display data
+        school.showStudents();
+        System.out.println();
+
+        s1.viewCourses();
+        s2.viewCourses();
+
+        System.out.println();
+
+        math.showStudents();
+        science.showStudents();
+    }
+}
