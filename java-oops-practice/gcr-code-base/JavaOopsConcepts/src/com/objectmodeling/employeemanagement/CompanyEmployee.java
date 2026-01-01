61a61d71a81c5b0af25440f8f06e0abeffa63a1c
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-01 13:32:23 +0530

    [Abuzar] : Added a program to implement a Employee Management System

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/employeemanagement/CompanyEmployee.java
@@ -0,0 +1,16 @@
+package com.objectmodeling.employeemanagement;
+
+public class CompanyEmployee {
+    private String name;
+    private int id;
+
+    public CompanyEmployee(String name, int id) {
+        this.name = name;
+        this.id = id;
+        System.out.println("Employee created: " + name);
+    }
+
+    public void displayEmployee() {
+        System.out.println("Employee ID: " + id + ", Name: " + name);
+    }
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/employeemanagement/CompanyMain.java
@@ -0,0 +1,29 @@
+package com.objectmodeling.employeemanagement;
+
+public class CompanyMain {
+    public static void main(String[] args) {
+
+        // Creating company object
+        Company company = new Company("Capgemini");
+
+        // Creating departments
+        CompanyDepartment dev = new CompanyDepartment("Development");
+        CompanyDepartment hr = new CompanyDepartment("HR");
+
+        // Adding employees to departments
+        dev.addEmployee("Abuzar", 101);
+        dev.addEmployee("Laxmi", 102);
+        hr.addEmployee("Arif", 201);
+
+        // Adding departments to company
+        company.addDepartment(dev);
+        company.addDepartment(hr);
+
+        // Display full structure
+        company.showCompanyStructure();
+
+        // Closing the company (composition effect)
+        company.closeCompany();
+    }
+}
+
