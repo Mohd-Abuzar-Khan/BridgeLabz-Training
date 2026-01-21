746373e7ccca776e1aa4c7921c95165bf6197482
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 10:03:58 +0530

    [Abuzar] : Added a program to sort list of tickets using quick sort

---
+++ dsa-java-practice/scenario-based/sorting/eventmanager/Ticket.java
@@ -0,0 +1,14 @@
+package sorting.eventmanager;
+
+//Class Ticket to store details of ticket
+public class Ticket {
+	String eventName;
+	int price;
+
+	// Parameterized constructor
+	public Ticket(String eventName, int price) {
+		this.eventName = eventName;
+		this.price = price;
+	}
+
+}
