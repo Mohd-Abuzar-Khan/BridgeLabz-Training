8ccf6d44b6d7ee5be24060e2a8262eac04b0e14f
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 11:36:17 +0530

    [Abuzar] : Added a program to help make a meal managment system using generics

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/mealplan/Meal.java
@@ -0,0 +1,23 @@
+package com.generics.mealplan;
+
+//Generic Meal class restricted to MealPlan implementations
+class Meal<T extends MealPlan> {
+
+	// Selected meal plan
+	private T mealPlan;
+
+	// Constructor to initialize meal plan
+	public Meal(T mealPlan) {
+		this.mealPlan = mealPlan;
+	}
+
+	// Returns meal plan
+	public T getMealPlan() {
+		return mealPlan;
+	}
+
+	// Displays meal details
+	public void displayMeal() {
+		System.out.println("Meal Type: " + mealPlan.getMealType() + ", Calories: " + mealPlan.getCalories());
+	}
+}
