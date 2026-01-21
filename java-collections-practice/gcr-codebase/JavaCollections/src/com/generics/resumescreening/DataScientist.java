4c30003698441d1f0265735a9274b75230cec36b
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 16:50:17 +0530

    [Abuzar] : Added a program for an AI-Driven Resume Screening System

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/generics/resumescreening/DataScientist.java
@@ -0,0 +1,15 @@
+package com.generics.resumescreening;
+
+//Data Scientist job role
+class DataScientist extends JobRole {
+
+	// Returns role name
+	public String getRoleName() {
+		return "Data Scientist";
+	}
+
+	// Returns minimum experience
+	public int getMinExperience() {
+		return 3;
+	}
+}
