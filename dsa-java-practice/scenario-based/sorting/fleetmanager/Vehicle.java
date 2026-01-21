592bf297a6422384067554c6fe88e64f4bfcbbaf
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 10:06:19 +0530

    [Abuzar] : Added a program to sort list of vehicles using merge sort

---
+++ dsa-java-practice/scenario-based/sorting/fleetmanager/Vehicle.java
@@ -0,0 +1,18 @@
+package sorting.fleetmanager;
+
+public class Vehicle {
+	
+	String brand;
+	String model;
+	int mileage;
+	String depot;
+	public Vehicle(String brand, String model, int mileage, String depot) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.mileage = mileage;
+		this.depot = depot;
+	}
+	
+	
+}
