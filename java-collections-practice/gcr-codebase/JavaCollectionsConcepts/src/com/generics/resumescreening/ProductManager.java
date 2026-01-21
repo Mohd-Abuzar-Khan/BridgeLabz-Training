a9c6395848b2c0695dc4d2ade8c2837fe8420ad4
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 11:38:21 +0530

    [Abuzar] : Added a program to make a resume screening application using generics

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/resumescreening/ProductManager.java
@@ -0,0 +1,15 @@
+package com.generics.resumescreening;
+
+//Product Manager job role
+class ProductManager extends JobRole {
+
+	// Returns role name
+	public String getRoleName() {
+		return "Product Manager";
+	}
+
+	// Returns minimum experience
+	public int getMinExperience() {
+		return 4;
+	}
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/resumescreening/SoftwareEngineer.java
@@ -0,0 +1,15 @@
+package com.generics.resumescreening;
+
+//Software Engineer job role
+class SoftwareEngineer extends JobRole {
+
+	// Returns role name
+	public String getRoleName() {
+		return "Software Engineer";
+	}
+
+	// Returns minimum experience
+	public int getMinExperience() {
+		return 2;
+	}
+}
