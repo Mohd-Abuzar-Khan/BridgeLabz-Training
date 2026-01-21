d66f8ca4a06807cafff690edae89d34fc2a39f20
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 10:14:07 +0530

    [Abuzar] : Added a program to sort list of items using merge sort

---
+++ dsa-java-practice/scenario-based/sorting/zipzipmart/Transactions.java
@@ -0,0 +1,14 @@
+package sorting.zipzipmart;
+import java.time.LocalDate;
+
+public class Transactions {
+	
+	LocalDate date;
+	double amount;
+	
+	public Transactions(LocalDate date, double amount) {
+		this.date = date;
+		this.amount = amount;
+	}
+
+}
