b4976e2c495aa0cce007ce41ac76bd4fff5d4038
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 09:52:17 +0530

    [Abuzar] : Added a program to implement a queue of Paitents in a Hospital

---
+++ dsa-java-practice/scenario-based/hospitalqueue/Patient.java
@@ -0,0 +1,17 @@
+package hospitalqueue;
+
+//Class to store patient details
+public class Patient {
+	// Instance of patient
+	String name;
+	int criricalLevel;
+
+	// Parameterized Constructor
+	public Patient(String name, int criricalLevel) {
+
+		this.name = name;
+		this.criricalLevel = criricalLevel;
+
+	}
+
+}
