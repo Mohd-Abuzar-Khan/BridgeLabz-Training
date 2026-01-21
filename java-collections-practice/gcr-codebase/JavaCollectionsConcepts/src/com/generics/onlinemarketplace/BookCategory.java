9d7d4a538fa92baec6f1327f75aae6ed116936ed
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 11:37:30 +0530

    [Abuzar] : Added a program to make a online market place using generics

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/onlinemarketplace/BookCategory.java
@@ -0,0 +1,10 @@
+package com.generics.onlinemarketplace;
+
+//Represents book-related products
+class BookCategory implements Category {
+
+	// Returns category name
+	public String getCategoryName() {
+		return "Books";
+	}
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/onlinemarketplace/Category.java
@@ -0,0 +1,7 @@
+package com.generics.onlinemarketplace;
+
+//Common interface for all product categories
+interface Category {
+	// Returns the category name
+	String getCategoryName();
+}
