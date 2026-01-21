9eb0600360c756fc02d203e67b8e0434115d616f
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-01-21 10:05:21 +0530

    [Abuzar] : Added a program to sort list of scores from multiple test centers using merge sort

---
+++ dsa-java-practice/scenario-based/sorting/examcell/Student.java
@@ -0,0 +1,17 @@
+package sorting.examcell;
+
+///Class Student to store details of students
+public class Student {
+	String name;
+	int score;
+	String testCenter;
+
+	// Parameterized Constructor
+	public Student(String name, int score, String testCenter) {
+
+		this.name = name;
+		this.score = score;
+		this.testCenter = testCenter;
+	}
+
+}
