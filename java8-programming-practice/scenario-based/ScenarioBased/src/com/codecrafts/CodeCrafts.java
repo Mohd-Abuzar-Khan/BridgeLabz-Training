b39045e08fbb84b4374e5851c3a4e5aeb06d7490
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-02-17 10:38:02 +0530

    [Abuzar] : Added a program for practicing String manipulation

---
+++ java8-programming-practice/scenario-based/ScenarioBased/src/com/codecrafts/CodeCrafts.java
@@ -0,0 +1,56 @@
+package com.codecrafts;
+
+import java.util.Scanner;
+
+public class CodeCrafts {
+	
+	public static Submission validateSubmission(String submissionCode) {
+		
+		
+		String pattern = "^CC-([A-Z]{3}[0-9]{2}[A-Z])-([A-Z]{5}[0-9]{2})-";
+		
+		
+//		String[] arr = submissionCode.split("-");
+//		String userID = null;
+//		String projectCode = null;
+//		int duration = 0;
+//		int score = 0;
+//		
+//		Submission s = null;
+//		
+//		if(arr[1].matches("^[A-Z]{3}[0-9]{2}[A-Z]{1}") || arr[2].matches("[A-Z]{5}[0-9]{2} ") || arr[3].matches("^[0-9]{5}[M*H]") || arr[4].matches("[0-9]{4}")) {
+//			userID = arr[1];
+//			projectCode = arr[2];
+//			String time = arr[3];
+//			if (time.charAt(1) == 'H') 
+//			{
+//				duration = (time.charAt(0) - '0') * 60;
+//			} else {
+//				duration = time - "0";
+//			}
+//			duration = Integer.parseInt(arr[3]);
+//			
+//			score = Integer.parseInt(arr[4]);
+//		} else {
+//			return s;
+//		}
+//		
+//		s = new Submission(userID, projectCode, duration, score);
+//		
+//		return s;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		
+		Submission submit  = validateSubmission(input);
+		
+		if (submit == null) {
+			System.out.println("Invalid Submission Format");
+		}
+	}
+
+}
---
+++ java8-programming-practice/scenario-based/ScenarioBased/src/com/codecrafts/Submission.java
@@ -0,0 +1,18 @@
+package com.codecrafts;
+
+public class Submission {
+	String userId;
+	String projectCode;
+	int durationInMinutes;
+	int score;
+	
+	public Submission(String userId, String projectCode, int durationInMinutes, int score) {
+		this.userId = userId;
+		this.projectCode = projectCode;
+		this.durationInMinutes = durationInMinutes;
+		this.score = score;
+	}
+	
+	
+
+}
